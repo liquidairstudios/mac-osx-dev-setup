d15a71c230e3a32884983809cb54ac79c3d3c018
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-06-27 14:14:54 +0100

    add bash aliases

---
+++ scripts/aliases.bash
@@ -0,0 +1,14 @@
+export JAVA_HOME=/usr/libexec/java_home
+export GOPATH=~/Workspace/go
+export PATH=$PATH:$GOPATH/bin
+
+alias dash="open dash://$1"
+alias gst='git status'
+alias gup="git pull --rebase"
+alias gp="git push"
+alias gap="git add -p"
+alias duet='git duet --global'
+alias solo='git solo --global'
+alias be='bundle exec'
+alias bx='be'
+alias show="ps auxww | grep -v grep | grep "
