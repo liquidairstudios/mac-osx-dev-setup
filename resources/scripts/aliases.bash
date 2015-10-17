057794d21e72afd5ea4ee3f3dacb81d013cda3d1
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-10-17 01:21:20 +0100

    Add alias to fix duplicated Open with items

--- resources/scripts/aliases.bash
+++ resources/scripts/aliases.bash
@@ -9,3 +9,4 @@ alias be='bundle exec'
 alias bx='be'
 alias show="ps auxww | grep -v grep | grep "
 alias vim=nvim
+alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
