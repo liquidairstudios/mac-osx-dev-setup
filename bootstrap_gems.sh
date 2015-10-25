ed0bd1af8dc2fa33d73450d70fa7bcbb0c8f2f7a
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-10-25 15:57:04 +0000

    Add rvm and rails setup

--- bootstrap_gems.sh
+++ bootstrap_gems.sh
@@ -3,10 +3,12 @@
 
 source ./bootstrap_utils.sh
 
+curl -sSL https://get.rvm.io | bash -s stable
 ruby-install ruby 2.1.7 --no-reinstall
 chruby ruby-2.1.7
 
 gem install bundler
 gem install tmuxinator
+gem install rails
 
 symlink_resource pryrc 
