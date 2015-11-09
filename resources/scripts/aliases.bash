c89a3c4cfbb253147e08aade60140281bf9d6139
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-11-09 21:51:48 +0000

    Organise aliases

--- resources/scripts/aliases.bash
+++ resources/scripts/aliases.bash
@@ -10,3 +10,4 @@ alias bx='be'
 alias show="ps auxww | grep -v grep | grep "
 alias vim=nvim
 alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
+alias flushdns='sudo discoveryutil udnsflushcaches'
