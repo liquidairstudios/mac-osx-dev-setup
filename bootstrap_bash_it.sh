d017615b7bc9bbd579dcf5931581abb861c70fb3
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-12-01 08:40:13 +0000

    Configure default bash or fish shell

--- bootstrap_bash_it.sh
+++ bootstrap_bash_it.sh
@@ -2,6 +2,8 @@
 
 source bootstrap_utils.sh
 
+chsh -s /usr/local/bin/bash
+
 clone Bash-it/bash-it .bash_it
 
 rm -rf $HOME/.bash_it/plugins/enabled/*
@@ -10,40 +12,40 @@ rm -rf $HOME/.bash_it/aliases/enabled/*
 
 symlink_resource bin
 symlink_resource bash_profile
-symlink_resource dir_colors 
+symlink_resource dir_colors
 
 symlink_bash_it_script aliases
 symlink_bash_it_theme ralch
 
 source $HOME/.bash_profile
 
-bash_it_enable_plugin ruby 
-bash_it_enable_plugin fasd 
-bash_it_enable_plugin ssh 
-bash_it_enable_plugin tmux 
-bash_it_enable_plugin osx 
-bash_it_enable_plugin battery 
-bash_it_enable_plugin base 
-bash_it_enable_plugin docker-machine 
+bash_it_enable_plugin ruby
+bash_it_enable_plugin fasd
+bash_it_enable_plugin ssh
+bash_it_enable_plugin tmux
+bash_it_enable_plugin osx
+bash_it_enable_plugin battery
+bash_it_enable_plugin base
+bash_it_enable_plugin docker-machine
 
 bash_it_enable_alias vim
 bash_it_enable_alias vagrant
 bash_it_enable_alias git
 bash_it_enable_alias rails
-bash_it_enable_alias homebrew 
-bash_it_enable_alias tmux 
-bash_it_enable_alias general 
+bash_it_enable_alias homebrew
+bash_it_enable_alias tmux
+bash_it_enable_alias general
 bash_it_enable_alias vagrant
 
-bash_it_enable_completion brew 
-bash_it_enable_completion defaults 
-bash_it_enable_completion gem 
-bash_it_enable_completion git 
-bash_it_enable_completion gulp 
-bash_it_enable_completion npm 
-bash_it_enable_completion packer 
-bash_it_enable_completion pip 
-bash_it_enable_completion rake 
-bash_it_enable_completion ssh 
-bash_it_enable_completion tmux 
-bash_it_enable_completion vagrant 
+bash_it_enable_completion brew
+bash_it_enable_completion defaults
+bash_it_enable_completion gem
+bash_it_enable_completion git
+bash_it_enable_completion gulp
+bash_it_enable_completion npm
+bash_it_enable_completion packer
+bash_it_enable_completion pip
+bash_it_enable_completion rake
+bash_it_enable_completion ssh
+bash_it_enable_completion tmux
+bash_it_enable_completion vagrant
--- bootstrap_omf.sh
+++ bootstrap_omf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO: Ask the user which shell he want
-# chsh -s /usr/local/bin/fish
+chsh -s /usr/local/bin/fish
+
 OMF_CONFIG=$PWD/resources/omf CI=true fish <(curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install) || true
 
